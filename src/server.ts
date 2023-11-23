2196054f45fb9121976027d07dfb9928b5badfc1
Md.Tanvir-Hossain-khondoker <tanvirkhondo21@gmail.com>
2023-11-23 21:53:43 +0600

    dot env confirgured

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
-const mongoose = require('mongoose');
+import config from "./app/config";
+
+const mongoose = require("mongoose");
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test');
-  
-
-  }
+  await mongoose.connect(config.database_url);
+}
 
 
 
 
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+
+
+app.listen(config.port, () => {
+  console.log(`Example app listening on port ${config.port}`);
+});
