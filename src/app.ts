bf407714df44f86304dabda53154f60dcfd6c202
Md.Tanvir-Hossain-khondoker <tanvirkhondo21@gmail.com>
2023-11-24 00:33:44 +0600

    eslint setup done

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import express, { Application, Request, Response } from 'express'
 const app:Application = express()
 import cors from 'cors'
-const port = 3000
 
 //parser 
 app.use(express.json())
 app.use(cors())
 
 app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!')
+   
+   
+   
+    const a = 10;
+  res.send(a);
 })
 
 
