c7f5ba483d9f6b72e395ee89793d07c41f4fd8be
Md.Tanvir-Hossain-khondoker <tanvirkhondo21@gmail.com>
2023-11-24 00:16:54 +0600

    server.ts and app.ts initial done

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,15 @@
+import app from "./app";
 import config from "./app/config";
 
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 async function main() {
-  await mongoose.connect(config.database_url);
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
-
-
-
-
-
-
-
-app.listen(config.port, () => {
-  console.log(`Example app listening on port ${config.port}`);
-});
