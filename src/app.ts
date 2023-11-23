31ca5afde6606734608f718d12bc5fbc0053981e
Md.Tanvir-Hossain-khondoker <tanvirkhondo21@gmail.com>
2023-11-24 00:49:37 +0600

    prettier setup done

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,14 @@
 import express, { Application, Request, Response } from 'express'
-const app:Application = express()
+const app: Application = express()
 import cors from 'cors'
 
-//parser 
+//parser
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req:Request, res:Response) => {
-   
-   
-   
-    const a = 10;
-  res.send(a);
+app.get('/', (req: Request, res: Response) => {
+  const a = 10
+  res.send(a)
 })
 
-
-
-export default app;
+export default app
